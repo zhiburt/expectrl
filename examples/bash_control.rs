3d0969de2f4d4cd361318b94f34eed0878604aed
Maxim Zhiburt <zhiburt@gmail.com>
2021-07-12 16:25:55 +0300

    Fix bash_control example
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

--- examples/bash_control.rs
+++ examples/bash_control.rs
@@ -27,22 +27,24 @@ fn main() -> Result<(), Error> {
 
 #[cfg(feature = "async")]
 fn main() -> Result<(), Error> {
-    let mut p = spawn_bash().await?;
-    p.send_line("ping 8.8.8.8").await?;
-    p.expect("bytes of data").await?;
-    p.send_control(ControlCode::Substitute).await?; // CTRL_Z
-    p.expect_prompt().await?;
-    // bash writes 'ping 8.8.8.8' to stdout again to state which job was put into background
-    p.send_line("bg").await?;
-    p.expect("ping 8.8.8.8").await?;
-    p.expect_prompt().await?;
-    p.send_line("sleep 0.5").await?;
-    p.expect_prompt().await?;
-    // bash writes 'ping 8.8.8.8' to stdout again to state which job was put into foreground
-    p.send_line("fg").await?;
-    p.expect("ping 8.8.8.8").await?;
-    p.send_control(ControlCode::EndOfText).await?;
-    p.expect("packet loss").await?;
+    futures_lite::future::block_on(async {
+        let mut p = spawn_bash().await?;
+        p.send_line("ping 8.8.8.8").await?;
+        p.expect("bytes of data").await?;
+        p.send_control(ControlCode::Substitute).await?; // CTRL_Z
+        p.expect_prompt().await?;
+        // bash writes 'ping 8.8.8.8' to stdout again to state which job was put into background
+        p.send_line("bg").await?;
+        p.expect("ping 8.8.8.8").await?;
+        p.expect_prompt().await?;
+        p.send_line("sleep 0.5").await?;
+        p.expect_prompt().await?;
+        // bash writes 'ping 8.8.8.8' to stdout again to state which job was put into foreground
+        p.send_line("fg").await?;
+        p.expect("ping 8.8.8.8").await?;
+        p.send_control(ControlCode::EndOfText).await?;
+        p.expect("packet loss").await?;
+    });
 
     Ok(())
 }
