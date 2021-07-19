25f95bd05dc9c123f02d6a9b7d9f1eac5db3825b
Maxim Zhiburt <zhiburt@gmail.com>
2021-07-19 19:22:35 +0300

    Fix cargo fmt
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

--- examples/interact.rs
+++ examples/interact.rs
@@ -8,7 +8,7 @@ fn main() {
 
     println!("Now you're in interacting mode");
     println!("To return control back to main type CTRL-]");
-    
+
     bash.interact().expect("Failed to start interact");
 
     println!("Quiting");
@@ -16,8 +16,7 @@ fn main() {
 
 #[cfg(feature = "async")]
 fn main() {
-    let mut bash =
-        futures_lite::future::block_on(spawn_bash()).expect("Error while spawning bash");
+    let mut bash = futures_lite::future::block_on(spawn_bash()).expect("Error while spawning bash");
 
     println!("Now you're in interacting mode");
     println!("To return control back to main type CTRL-]");
