2e445e4171d45f87a2c4d3867a7b041c6aeb55fe
Maxim Zhiburt <zhiburt@gmail.com>
2021-08-25 23:30:09 +0300

    Add example for expect Any
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

---
+++ examples/expect_line.rs
@@ -0,0 +1,52 @@
+// An example is based on README.md from https://github.com/philippkeller/rexpect
+
+use expectrl::{self, Any, Eof};
+
+#[cfg(feature = "sync")]
+fn main() {
+    let mut session = expectrl::spawn("ls -al").expect("Can't spawn a session");
+
+    loop {
+        let m = session
+            .expect(Any(vec![Box::new("\r"), Box::new("\n"), Box::new(Eof)]))
+            .expect("Expect failed");
+
+        let is_eof = m.found_match().is_empty();
+        if is_eof {
+            break;
+        }
+
+        if m.found_match() == [b'\n'] {
+            continue;
+        }
+
+        println!("{:?}", String::from_utf8_lossy(m.before_match()));
+    }
+}
+
+#[cfg(feature = "async")]
+fn main() {
+    use futures_lite::io::AsyncBufReadExt;
+
+    futures_lite::future::block_on(async {
+        let mut session = expectrl::spawn("ls -al").expect("Can't spawn a session");
+
+        loop {
+            let m = session
+                .expect(Any(vec![Box::new("\r"), Box::new("\n"), Box::new(Eof)]))
+                .await
+                .expect("Expect failed");
+    
+            let is_eof = m.found_match().is_empty();
+            if is_eof {
+                break;
+            }
+    
+            if m.found_match() == [b'\n'] {
+                continue;
+            }
+    
+            println!("{:?}", String::from_utf8_lossy(m.before_match()));
+        }
+    })
+}
