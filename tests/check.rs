71f0aa6aaf018edad049fcc519d458fe97acafe0
Maxim Zhiburt <zhiburt@gmail.com>
2021-09-25 12:15:08 +0300

    Fix cargo clippy for async version
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

--- tests/check.rs
+++ tests/check.rs
@@ -382,41 +382,37 @@ fn check_macro_with_different_needles() {
         session.send_line("Hello World").await.unwrap();
 
         thread::sleep(Duration::from_millis(600));
-        async {
-            expectrl::check!(
-                session,
-                number = Any(["123", "345"]) => {
-                    assert_eq!(number.first(), b"345")
-                },
-                line = "\n" => {
-                    assert_eq!(line.before(), b"Hello World\r")
-                },
-                default => {
-                    panic!("Unexpected result");
-                },
-            )
-            .await
-            .unwrap();
-        };
+        expectrl::check!(
+            session,
+            number = Any(["123", "345"]) => {
+                assert_eq!(number.first(), b"345")
+            },
+            line = "\n" => {
+                assert_eq!(line.before(), b"Hello World\r")
+            },
+            default => {
+                panic!("Unexpected result");
+            },
+        )
+        .await
+        .unwrap();
 
         session.send_line("345").await.unwrap();
 
         thread::sleep(Duration::from_millis(600));
-        async {
-            expectrl::check!(
-                session,
-                number = Any(["123", "345"]) => {
-                    assert_eq!(number.first(), b"345")
-                },
-                line = "\n" => {
-                    assert_eq!(line.before(), b"Hello World\r")
-                },
-                default => {
-                    panic!("Unexpected result");
-                },
-            )
-            .await
-            .unwrap();
-        };
+        expectrl::check!(
+            session,
+            number = Any(["123", "345"]) => {
+                assert_eq!(number.first(), b"345")
+            },
+            line = "\n" => {
+                assert_eq!(line.before(), b"Hello World\r")
+            },
+            default => {
+                panic!("Unexpected result");
+            },
+        )
+        .await
+        .unwrap();
     });
 }
