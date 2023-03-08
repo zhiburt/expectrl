c3b82419eabe16cefe446471737b58f26b09f0b9
Maxim Zhiburt <zhiburt@gmail.com>
2023-03-09 01:11:09 +0300

    s.d......
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

--- tests/session.rs
+++ tests/session.rs
@@ -86,6 +86,7 @@ fn send_multiline() {
         let mut session = spawn("cat").unwrap();
         session.send("Hello World\n").await.unwrap();
 
+        thread::sleep(Duration::from_millis(300));
         session.write_all(&[3]).await.unwrap(); // Ctrl+C
         session.flush().await.unwrap();
 
